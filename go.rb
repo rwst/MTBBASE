0033ccdc21bc2db66c0199e2ad47114da518bc0b
R. Stephan <ralf@ark.in-berlin.de>
2010-08-04 17:41:41 +0200

    Set db to TBGO, replace rwst

--- go.rb
+++ go.rb
@@ -4,8 +4,10 @@ require 'bio'
 data = File.open('TBGO.tab').read
 Bio::GO::Phenote_GOA.parser(data) do |entry|
   str = entry.to_str
+  str = str.sub(/rwst1/, 'ralf@ark.in-berlin.de')
+  str = str.sub(/rwst/, 'ralf@ark.in-berlin.de')
   unless entry.taxon !~ /organism:1773/      # only M.tb.
-    p str
+    p "TBGO" + str
   end
 end
 
