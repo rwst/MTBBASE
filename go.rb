c906dd709a6d1d227609136d1f2e4a36d1583563
R. Stephan <ralf@ark.in-berlin.de>
2010-08-06 18:43:16 +0200

    warn if no reciprocal protein binding annotation found

--- go.rb
+++ go.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby -w
 require 'bio'
 
+h = Hash.new
 data = File.open('TBGO.tab').read
 Bio::GO::Phenote_GOA.parser(data) do |entry|
   entry.db = 'UniProt'
@@ -9,9 +10,16 @@ Bio::GO::Phenote_GOA.parser(data) do |entry|
     $stderr.print 'Error: IEP+P violated:' + "\n"
     $stderr.print(entry.to_str + "\n")
   end
-  if entry.goid == '0005515' and entry.with.empty? then
-    $stderr.print 'Error: protein binding+with violated:' + "\n"
-    $stderr.print(entry.to_str + "\n")
+  if entry.goid == '0005515' then
+    if entry.with.empty? then
+      $stderr.print 'Error: protein binding+with violated:' + "\n"
+      $stderr.print(entry.to_str + "\n")
+    else
+      key = entry.db_object_id.to_s + "\t" +
+            entry.with.join('|') + "\t" +
+            entry.db_reference.join('|')
+      h[key] = true
+    end
   end
   unless entry.taxon !~ /organism:1773/      # only M.tb.
     entry.taxon = 'taxon:1773'
@@ -21,4 +29,13 @@ Bio::GO::Phenote_GOA.parser(data) do |entry|
     $stdout.print(entry.to_str + "\n")
   end
 end
+
+  h.each_key {|key|
+    tmp = key.chomp.split(/\t/)
+    revkey = tmp[1] + "\t" + tmp[0] + "\t" + tmp[2]
+    if !h.has_key?(revkey)
+      $stderr.print 'Warning: protein binding reverse entry missing:' + "\n"
+      $stderr.print(revkey + "\n")
+    end
+  }
 
