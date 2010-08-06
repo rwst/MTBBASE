87db052cf01e543f0a49c28e34a094755850ba8f
R. Stephan <ralf@ark.in-berlin.de>
2010-08-06 16:14:48 +0200

    add flagging of IEP+P error

--- go.rb
+++ go.rb
@@ -5,6 +5,10 @@ data = File.open('TBGO.tab').read
 Bio::GO::Phenote_GOA.parser(data) do |entry|
   entry.db = 'UniProt'
   entry.assigned_by = 'ralf@ark.in-berlin.de'
+  if entry.evidence == 'IEP' and entry.aspect != 'P' then
+    $stderr.print 'Error: IEP+P violated:' + "\n"
+    $stderr.print(entry.to_str + "\n")
+  end
   unless entry.taxon !~ /organism:1773/      # only M.tb.
     entry.taxon = 'taxon:1773'
     if entry.date =~ /20100701[3-8]/
