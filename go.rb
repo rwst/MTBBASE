157e9699aa5d02b886905d1d7812456407cff435
R. Stephan <ralf@ark.in-berlin.de>
2010-08-07 19:18:15 +0200

    implement renaming from Rv to UniProt, including synonyms and with field

--- go.rb
+++ go.rb
@@ -1,6 +1,46 @@
 #!/usr/bin/env ruby -w
 require 'bio'
 
+# first read synonyms from UniProt flat file
+syn = Hash.new
+synfile = File.open('1773.txt').read
+$stderr.print("Reading names from UniProt file...\n")
+id = ac = ""
+names = []
+synfile.each_line("\n") {|line|
+  next if /^ID|^AC|^GN/ !~ line
+  case line[0,2]
+    when 'ID'
+      id = line.sub(/ID +/, '').sub(/ +.*\n/, '')
+    when 'AC'
+      ac = line.sub(/AC +/, '').sub(/[,;].*\n/, '')
+    when 'GN'
+      key = ""
+      gn = line.chomp.split(/[= ,;\/]/)
+      names = []
+      gn.each {|word|
+        if !word.empty? and
+           word !~ /GN|Name|Synonyms|OrderedLocusNames|ORFNames/ then
+          if word =~ /^Rv/
+            key = word
+#            $stderr.print("'" + key + "'\n")
+          else
+            names << word
+          end
+        end }
+      if !key.empty? and !names.include?(key) then names << key end
+#      $stderr.print("Log: Unknown RvID: '#{names.join('|')}', size:#{names.size}\n")
+      names.each {|name|
+        if name =~ /Rv/
+          if syn.has_key?(name)
+            $stderr.print("Error: duplicate name: " + name + "\n")
+          end
+          syn[name] = names << ac << id
+        end }
+    else $stderr.print("Can't happen: " + line[0,2] + "\n")
+  end
+  }
+
 h = Hash.new
 data = File.open('TBGO.tab').read
 Bio::GO::Phenote_GOA.parser(data) do |entry|
@@ -26,6 +66,29 @@ Bio::GO::Phenote_GOA.parser(data) do |entry|
     if entry.date =~ /20100701[3-8]/
       entry.date = entry.date.sub(/20100701([3-8])/, '2010071\1')
     end
+    obid = entry.db_object_id
+    if obid =~ /^Rv/
+      if !syn.has_key?(obid)
+        $stderr.print("Error: Unknown RvID: '" + obid + "'\n")
+      else
+        syns = syn[obid]
+        id = syns[-1]
+        ac = syns[-2]
+        syns = syns[0..-3]
+        entry.db_object_id = 'UniProt:' + ac
+        entry.db_object_symbol = id
+        entry.db_object_synonym = syns.flatten
+      end
+    end
+    with = entry.with
+    if with =~ /^Rv/
+      if !syn.has_key?(with)
+        $stderr.print('Error: Unknown With RvID:' + with + "\n")
+      else
+        syns = syn[with]
+        entry.with = 'UniProt:' + syns[-2]
+      end
+    end
     $stdout.print(entry.to_str + "\n")
   end
 end
@@ -34,8 +97,8 @@ end
     tmp = key.chomp.split(/\t/)
     revkey = tmp[1] + "\t" + tmp[0] + "\t" + tmp[2]
     if !h.has_key?(revkey)
-      $stderr.print 'Warning: protein binding reverse entry missing:' + "\n"
-      $stderr.print(revkey + "\n")
+#      $stderr.print 'Warning: protein binding reverse entry missing:' + "\n"
+#      $stderr.print(revkey + "\n")
     end
   }
 
